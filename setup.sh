5435c38efcab97245bdefa6493558e609b3f2c9d
Lilian Mallardeau <lilianmallardeau@gmail.com>
2024-03-30 04:10:03 +0100

    snake_case to kebab-case for folders

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 cd $(dirname ${BASH_SOURCE[0]})
 
 if [[ $# -eq 0 ]]; then
-    for setup_script in setup_scripts/*; do
+    for setup_script in setup-scripts/*; do
         [[ -x $setup_script ]] || chmod +x "$setup_script"
         echo "Setting up $(basename "$setup_script")..."
         "$setup_script"
         echo
     done
 else
-    if [[ -f setup_scripts/$1 ]]; then
-        [[ -x setup_scripts/$1 ]] || chmod +x "setup_scripts/$1"
-        setup_script="setup_scripts/$1"
+    if [[ -f setup-scripts/$1 ]]; then
+        [[ -x setup-scripts/$1 ]] || chmod +x "setup-scripts/$1"
+        setup_script="setup-scripts/$1"
         shift
         "$setup_script" "$@"
     else
