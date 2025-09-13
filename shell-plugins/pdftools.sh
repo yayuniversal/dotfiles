ad785ce46738f4a4c696b57f3fab773d044e5ae3
Lilian Mallardeau <lilianmallardeau@gmail.com>
2025-09-13 02:30:06 +0200

    [pdftools] Make pdfcompress able to compress multiple files in one go

--- shell-plugins/pdftools.sh
+++ shell-plugins/pdftools.sh
@@ -1,14 +1,14 @@
 function pdfcompress {
 	if [[ $# -lt 1 ]]; then
-		echo "Usage: pdfcompress <input_file.pdf> [<output_file.pdf>]" 1>&2
+		echo "Usage: pdfcompress <input_file.pdf>..." 1>&2
 		return 1
 	fi
 
-	infile="$1"
-	outfile="$(basename "$1" .pdf).unlocked.pdf"
-	[ $# -ge 2 ] && outfile="$2";
-
-	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS="/ebook" -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$outfile" "$infile"
+	while (($#)); do
+		echo "Compressing $1..."
+		gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS="/ebook" -dNOPAUSE -dQUIET -dBATCH -sOutputFile="$(basename "$1" .pdf).compressed.pdf" "$1"
+		shift
+	done
 }
 
 function pdfunlock {
@@ -16,9 +16,11 @@ function pdfunlock {
 		echo "Usage: pdfunlock <input_file.pdf>..." 1>&2
 		return 1
 	fi
+
 	echo -n "Enter PDF password: "
 	read -s password
 	echo
+
 	while (($#)); do
 		echo "Unlocking $1..."
 		gs -q -sPDFPassword="$password" -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile="$(basename "$1" .pdf).unlocked.pdf" "$1"
