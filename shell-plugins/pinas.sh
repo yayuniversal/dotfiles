410340fe9b1d3c291e0350e9a0e7b2043cabd5e4
Lilian Mallardeau <lilianmallardeau@gmail.com>
2025-09-25 22:01:49 +0200

    [shell/pinas] naspull/naspush now sync root zfs dataset as well

--- shell-plugins/pinas.sh
+++ shell-plugins/pinas.sh
@@ -1,6 +1,6 @@
 alias setpinasdate='ssh -A pinas sudo date -s \"$(date -R)\"'
-alias naspull='syncoid --recursive --no-sync-snap --skip-parent --no-rollback pinas.wg:nas sandisk'
-alias naspush='syncoid --recursive --no-sync-snap --skip-parent --no-rollback sandisk pinas.wg:nas'
+alias naspull='syncoid --recursive --no-sync-snap --no-rollback pinas.wg:nas sandisk'
+alias naspush='syncoid --recursive --no-sync-snap --no-rollback sandisk pinas.wg:nas'
 
 function nas-zfs-snap {
     snapshot="snap_$(date +%F_%T)"
